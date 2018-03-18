7b5e0a8ae774a24121ab68d80c6d03d9e17e6efb
VanjaVlahovic <vlahovicvanja@gmail.com>
2018-03-18 15:10:35 +0100

    Uvedena klasa Menjacnica koja implementira interfejs

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.MenjacnicaInterface;
+
+public class Menjacnica implements MenjacnicaInterface {
+
+	@Override
+	public void dodajKurs(String naziv, Kurs kurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(String naziv, Kurs kurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs vratiKurs(String naziv, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
