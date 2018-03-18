59e400efb53f5a41b21bf342e87d2e81638f39db
VanjaVlahovic <vlahovicvanja@gmail.com>
2018-03-18 14:50:19 +0100

    Uvedena nova grana specifikacijainterfejsa

---
+++ Menjacnica/src/menjacnica/interfejs/MenjacnicaInterface.java
@@ -0,0 +1,11 @@
+package menjacnica.interfejs;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.Kurs;
+
+public interface MenjacnicaInterface {
+	public void dodajKurs (String naziv, Kurs kurs);
+	public void obrisiKurs (String naziv, Kurs kurs);
+	public Kurs vratiKurs (String naziv, GregorianCalendar dan);
+}
